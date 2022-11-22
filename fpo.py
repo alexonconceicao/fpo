49b544b1a07bdc61e8d0d8a4b72f8ddb04aa5baf
Alexon Conceição <alexonoffice@gmail.com>
2022-11-22 15:32:32 -0300

    major changes in language and initial coding to language change

--- fpo.py
+++ fpo.py
@@ -6,46 +6,142 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
 configPath = '.\config'
-configFile = '.\config\locations.json'
+configFile = '.\config\config.json'
+locationsFile = '.\config\locations.json'
 
 
-def programasEncontrados(configFile):
-    with open(configFile, 'r', encoding='utf-8') as cf:
-        programasEncontrados = json.load(cf)
-    return programasEncontrados
+def programsFound(locationsFile):
+    with open(locationsFile, 'r', encoding='utf-8') as cf:
+        programsFound = json.load(cf)
+    return programsFound
 
 
-def buscarCaminhoPrograma(nome, configFile):
-    for p in programasEncontrados(configFile)['programas']:
-        if nome == p['nome']:
-            caminho = p['caminho']
-    return caminho
+def findProgramPath(filename, locationsFile):
+    for p in programsFound(locationsFile)['programs']:
+        if filename == p['name']:
+            filepath = p['path']
+    return filepath
 
 
-def mainWindow():
+def addProgram(window):
 
-    vetorNomeArquivo = []
+    menubar, askfilepath, askfilename = languageChooser()
+
+    filepath = sg.popup_get_file(askfilepath, no_window=True)
+    filename = sg.popup_get_text(title='FPO', message=askfilename)
+
+    print(filename)
+
+    if filepath == '':
+        return
+
+    if filename == '' or filename is None:
+        filename = os.path.basename(filepath).capitalize().replace('.exe', '')
+
+    entry = {
+        'name': filename,
+        'path': filepath,
+    }
+
+    if filepath not in programsFound(locationsFile):
+
+        with open(locationsFile, 'r+', encoding='utf-8') as cf:
+            data = json.load(cf)
+            data['programs'].append(entry)
+            cf.seek(0)
+            json.dump(data, cf, indent=4, ensure_ascii=False)
+            arrayNewFilenames = []
+            for p in data['programs']:
+                newFilename = p['name']
+                arrayNewFilenames.append(newFilename)
+
+            window['-LBPROGRAM-'].update(values=arrayNewFilenames)
+
+
+def buttonOpen(values):
+    if not values['-LBPROGRAM-']:
+        sg.popup_error('Por favor, selecione um item!')
+    else:
+        nomePrograma = values['-LBPROGRAM-'][0]
+        caminhoPrograma = findProgramPath(nomePrograma, locationsFile)
+        if caminhoPrograma.endswith('.exe'):
+            sp.Popen(
+                [caminhoPrograma],
+                creationflags=sp.DETACHED_PROCESS | sp.CREATE_NEW_PROCESS_GROUP,
+            )
+        else:
+            os.startfile(caminhoPrograma)
+
+
+def configIni():
+
+    arrayFileName = []
+
+    if not os.path.exists(locationsFile):
+        pattern = {"programs": []}
+        if not os.path.exists(configPath):
+            os.mkdir(configPath)
+        with open(locationsFile, 'w', encoding='utf-8') as cf:
+            json.dump(pattern, cf)
+
+    for p in programsFound(locationsFile)['programs']:
+        filename = p['name']
+        arrayFileName.append(filename)
+
+    return arrayFileName
+
+
+def languageChooser(flag=0):
+
+    language = ['ptBR', 'enUS']
 
     if not os.path.exists(configFile):
-        dict = {"programas": []}
+        pattern = {"language": language[flag]}
         if not os.path.exists(configPath):
             os.mkdir(configPath)
         with open(configFile, 'w', encoding='utf-8') as cf:
-            json.dump(dict, cf)
+            json.dump(pattern, cf)
 
-    for p in programasEncontrados(configFile)['programas']:
-        nomeArquivo = p['nome']
-        vetorNomeArquivo.append(nomeArquivo)
+    askFilepath_ptBR = 'Adicionar programa'
+    askFilepath_enUS = 'Add program'
 
-    menubar = [
+    askFilename_ptBR = 'Insira nome personalizado do arquivo:'
+    askFilename_enUS = 'Insert a customized filename:'
+
+    menubar_ptBR = [
         ['&Arquivo', ['&Adicionar programa']],
         ['&Editar', ['&Editar configuração']],
     ]
 
+    menubar_enUS = [
+        ['&Arquivo', ['&Add program']],
+        ['&Editar', ['&Editar configuração']],
+    ]
+
+    with open(configFile, 'r', encoding='utf-8') as cf:
+        langs = json.load(cf)
+
+    if langs['language'] == 'ptBR':
+        menubar = menubar_ptBR
+        askFilename = askFilename_ptBR
+        askFilepath = askFilepath_ptBR
+    else:
+        menubar = menubar_enUS
+        askFilename = askFilename_enUS
+        askFilepath = askFilepath_enUS
+
+    return menubar, askFilepath, askFilename
+
+
+def mainWindow():
+
+    arrayFileName = configIni()
+    menubar, askFilepath, askFilename = languageChooser()
+
     layout = [
         [sg.Menu(menubar, tearoff=False, pad=(200, 1))],
         [sg.T('Fast Program Opener')],
-        [sg.LB(vetorNomeArquivo, enable_events=True, key='-LBPROGRAM-', size=(25, 10))],
+        [sg.LB(arrayFileName, enable_events=True, key='-LBPROGRAM-', size=(25, 10))],
         [sg.RButton('Abrir', key='-BOPEN-', size=(19, 1))],
     ]
 
@@ -55,54 +151,12 @@ def mainWindow():
         event, values = window.read()
         if event == sg.WIN_CLOSED:
             break
-        # TODO adicionar função para editar arquivo de locations (configFile)
-        if event == 'Adicionar programa':
-
-            caminhoDoArquivo = sg.popup_get_file('Adicionar programa', no_window=True)
-            nomeDoArquivo = sg.popup_get_text(
-                title='FPO', message='Insira nome personalizado do arquivo:'
-            )
-
-            if caminhoDoArquivo == '':
-                continue
-
-            if nomeDoArquivo == '':
-                nomeDoArquivo = (
-                    os.path.basename(caminhoDoArquivo).capitalize().replace('.exe', '')
-                )
-
-            entry = {
-                'nome': nomeDoArquivo,
-                'caminho': caminhoDoArquivo,
-            }
-
-            if caminhoDoArquivo not in programasEncontrados(configFile):
-
-                with open(configFile, 'r+', encoding='utf-8') as cf:
-                    dados = json.load(cf)
-                    dados['programas'].append(entry)
-                    cf.seek(0)
-                    json.dump(dados, cf, indent=4, ensure_ascii=False)
-                    vetorNomeArquivosAtualizados = []
-                    for p in dados['programas']:
-                        nomeArquivo = p['nome']
-                        vetorNomeArquivosAtualizados.append(nomeArquivo)
-
-                    window['-LBPROGRAM-'].update(values=vetorNomeArquivosAtualizados)
-
+        # TODO adicionar função para editar arquivo de locations (locationsFile)
+        if event == 'Adicionar programa' or event == 'Add program':
+            addProgram(window)
         if event == '-BOPEN-':
-            if not values['-LBPROGRAM-']:
-                sg.popup_error('Por favor, selecione um item!')
-            else:
-                nomePrograma = values['-LBPROGRAM-'][0]
-                caminhoPrograma = buscarCaminhoPrograma(nomePrograma, configFile)
-                if caminhoPrograma.endswith('.exe'):
-                    sp.Popen(
-                        [caminhoPrograma],
-                        creationflags=sp.DETACHED_PROCESS | sp.CREATE_NEW_PROCESS_GROUP,
-                    )
-                else:
-                    os.startfile(caminhoPrograma)
+            buttonOpen(values)
+
     window.close()
 
 
